b0ea210d314c5eb88dcec604b534b649522b197d
D4C3R <d.c.madrigal02@gmail.com>
2025-01-25 12:40:01 -0600

    Estructura_beta_de_provedor

--- FE-Plataforma-de-servicios/src/Router/Rutas.jsx
+++ FE-Plataforma-de-servicios/src/Router/Rutas.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Login from "../pages/Login";
-import { Routes, Route } from 'react-router-dom';
-import  Proveedor_registro from "../pages/Proveedor_registro";
-import  Clientes_Registro  from "../pages/Clientes_registro";
+import ProveedorRegistro from "../pages/Proveedor_registro";
+import ClientesRegistro from "../pages/Clientes_registro";
+import ProvedorPage from "../pages/Provedor_Page";
 
 const Rutas = () => {
   return (
-    <>
     <Routes>
-        <Route path="/" element={<Login />} /> 
-        <Route path="/provedores" element={<Proveedor_registro/>} /> 
-        <Route path="/clientes" element={<Clientes_Registro/>} /> 
+      <Route path="/" element={<Login />} />
+      <Route path="/proveedores" element={<ProveedorRegistro />} />
+      <Route path="/clientes" element={<ClientesRegistro />} />
+      <Route path="/beta" element={<ProvedorPage />} />
     </Routes>
-    </>
   );
 };
 
---
+++ FE-Plataforma-de-servicios/src/componentes/Provedores-beta/Provedores.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const provedores_panel = () => {
+  return <div classname="provedores_container">Hola Mundo</div>;
+};
+
+export default Provedores;
