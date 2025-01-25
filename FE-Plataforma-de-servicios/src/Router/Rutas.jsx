15ba9977bd237338f7e588e26184862b8c210521
D4C3R <d.c.madrigal02@gmail.com>
2025-01-25 17:17:40 -0600

    diseno_login_finalizado

--- FE-Plataforma-de-servicios/src/Router/Rutas.jsx
+++ FE-Plataforma-de-servicios/src/Router/Rutas.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+
 import Login from "../pages/Login";
-import ProveedorRegistro from "../pages/Proveedor_registro";
-import ClientesRegistro from "../pages/Clientes_registro";
-import ProvedorPage from "../pages/Provedor_Page";
+import { Routes, Route } from 'react-router-dom';
+import  Proveedor_registro from "../pages/Proveedor_registro";
+import  Clientes_Registro  from "../pages/Clientes_registro";
 
 const Rutas = () => {
   return (
+    <>
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/proveedores" element={<ProveedorRegistro />} />
-      <Route path="/clientes" element={<ClientesRegistro />} />
-      <Route path="/beta" element={<ProvedorPage />} />
+        <Route path="/" element={<Login />} /> 
+        <Route path="/provedores" element={<Proveedor_registro/>} /> 
+        <Route path="/clientes" element={<Clientes_Registro/>} /> 
     </Routes>
+    </>
   );
 };
 
--- FE-Plataforma-de-servicios/src/componentes/Login/Login_form.jsx
+++ FE-Plataforma-de-servicios/src/componentes/Login/Login_form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import Button from "@mui/material/Button";
@@ -56,7 +56,7 @@ const Login_form = () => {
           error={formik.touched.password && Boolean(formik.errors.password)}
           helperText={formik.touched.password && formik.errors.password}
         />
-        <Button color="primary" variant="contained" fullWidth type="submit">
+        <Button className='Submit-button' variant="contained" fullWidth type="submit">
           Submit
         </Button>
         <div className="links-container">
@@ -68,5 +68,140 @@ const Login_form = () => {
   );
 };
 
-export default Login_form;
+export default Login_form; */
 
+
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import './login.css';
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
+
+const Login_form = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: 'foobar@example.com',
+      password: 'foobar',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <Container
+      className="login-container"
+      sx={{
+        marginTop: 8,
+        marginBottom: 8,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        borderRadius: 2,
+        bgcolor: '#ffffff',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          maxWidth: 400,
+          bgcolor: '#777777',
+          padding: 4,
+          borderRadius: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="h2"
+          gutterBottom
+          sx={{
+            marginBottom: 4,
+            color: '#ffffff',
+          }}
+        >
+          Login
+        </Typography>
+        <form onSubmit={formik.handleSubmit} className="login-form" style={{ width: '100%' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                fullWidth
+                type="submit"
+                sx={{
+                  backgroundColor: '#333333',
+                  '&:hover': {
+                    backgroundColor: '#111111',
+                  },
+                }}
+              >
+                Submit
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" justifyContent="space-between">
+                <Link to="/provedores" className="link">
+                  Ir a Proveedores
+                </Link>
+                <Link to="/clientes" className="link">
+                  Ir a Clientes
+                </Link>
+              </Box>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login_form;
