0a0634cb4aa8bcaedf083552ab9792e8c1b9c5c9
Yacith-D <martinezyacith@gmail.com>
2025-01-25 14:44:24 -0600

    se realizo en el apartado de provedores sus diferentes servicios

--- FE-Plataforma-de-servicios/src/Router/Rutas.jsx
+++ FE-Plataforma-de-servicios/src/Router/Rutas.jsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login";
 import { Routes, Route } from 'react-router-dom';
 import  Proveedor_registro from "../pages/Proveedor_registro";
 import  Clientes_Registro  from "../pages/Clientes_registro";
+import Pagina_proveedor from "../pages/Pagina_proveedor";
 
 const Rutas = () => {
   return (
@@ -11,6 +12,7 @@ const Rutas = () => {
         <Route path="/" element={<Login />} /> 
         <Route path="/provedores" element={<Proveedor_registro/>} /> 
         <Route path="/clientes" element={<Clientes_Registro/>} /> 
+        <Route path="/pageprove" element={<Pagina_proveedor/>} /> 
     </Routes>
     </>
   );
