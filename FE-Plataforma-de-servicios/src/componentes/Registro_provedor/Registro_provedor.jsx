a550bb221721cce727924972069503608143bb9f
D4C3R <d.c.madrigal02@gmail.com>
2025-01-25 22:19:16 -0600

    W.I.P_Login_Design

--- FE-Plataforma-de-servicios/src/componentes/Registro_provedor/Registro_provedor.jsx
+++ FE-Plataforma-de-servicios/src/componentes/Registro_provedor/Registro_provedor.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import "./prove_login.css"; // Archivo CSS externo
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
-import User_services from "../../services/User_services";
-import Provider_services from "../../services/Provider_services";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import Provider_services from "../../services/Provider_services";
+import User_services from "../../services/User_services";
 
-// Validaciones con Yup
 const validationSchema = yup.object({
   email: yup
     .string("Enter your email")
@@ -28,12 +30,10 @@ const validationSchema = yup.object({
     .required("Name is required"),
 });
 
-const Login_provedor = () => {
+const Registro_provedor = () => {
   const [position, setPosition] = useState(null);
-
-  const no_location_toast = () => toast.error("Selecciona una ubicación");
- const user_added = () => toast.success("Usuario creado");
   const navigate = useNavigate();
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -41,40 +41,53 @@ const Login_provedor = () => {
       provider_name: "",
     },
     validationSchema: validationSchema,
-    onSubmit: (values) => {
-      
+    onSubmit: async (values) => {
       if (!position) {
-        no_location_toast();
+        toast.error("Selecciona una ubicación");
         return;
       }
 
-      post_user();
-      user_added();
-      setTimeout(() => {
-        navigate("/");
-      }, 3000); 
+      try {
+        const { provider_name, email, password } = values;
+        const new_provider = {
+          name: provider_name,
+          latitude: position.lat,
+          longitude: position.lng,
+        };
+
+        const provider_posted = await Provider_services.post_provider(
+          new_provider
+        );
+        if (!provider_posted) throw new Error("Error al añadir proveedor");
+
+        const new_user_id = provider_posted.provider.id;
+        const new_user = {
+          mail: email,
+          password: password,
+          rol: "Proveedor",
+          client_id: null,
+          provider_id: new_user_id,
+        };
+
+        await User_services.post_user(new_user);
+        toast.success("Usuario creado");
+        setTimeout(() => navigate("/"), 3000);
+      } catch (error) {
+        console.error("Error:", error);
+        toast.error("Error al registrar el usuario");
+      }
     },
   });
 
-  //Mapa de Leaflet
-  React.useEffect(() => {
-    //Mapa por defecto
+  useEffect(() => {
     const map = L.map("map").setView([9.9368, -84.0852], 8);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-
-    //Marcador
     let marker;
+    map.on("click", (e) => {
+      const { lat, lng } = e.latlng;
+      setPosition({ lat, lng });
 
-    // Capturar el Click
-    map.on("click", function (e) {
-      const { lat, lng } = e.latlng; // Obtener latitud y longitud
-      setPosition({ lat, lng }); // Guardar en el estado
-
-      // Añadir o mover el marcador al hacer clic
       if (marker) {
         marker.setLatLng([lat, lng]);
       } else {
@@ -82,127 +95,124 @@ const Login_provedor = () => {
       }
     });
 
-    // Cleanup del mapa
-    return () => {
-      map.remove();
-    };
+    return () => map.remove();
   }, []);
 
-  //Funcion que despliega el mapa
-  function Display_map() {
-    return (
-      <div>
-        <h4 id="map_tittle">Selecciona tu ubicación</h4>
-        <div
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        marginTop: 8,
+        marginBottom: 8,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "#ffffff",
+        borderRadius: 2,
+        boxShadow: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          maxWidth: 500,
+          bgcolor: "#777777",
+          padding: 4,
+          borderRadius: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="h2"
+          sx={{
+            marginBottom: 4,
+            color: "#ffffff",
+          }}
+        >
+          Registro Proveedor
+        </Typography>
+        <form onSubmit={formik.handleSubmit} style={{ width: "100%" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Correo"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Contraseña"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="provider_name"
+                name="provider_name"
+                label="Nombre"
+                value={formik.values.provider_name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.provider_name &&
+                  Boolean(formik.errors.provider_name)
+                }
+                helperText={
+                  formik.touched.provider_name && formik.errors.provider_name
+                }
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                fullWidth
+                type="submit"
+                sx={{
+                  backgroundColor: "#333333",
+                  "&:hover": {
+                    backgroundColor: "#111111",
+                  },
+                }}
+              >
+                Enviar
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+        <Box
           id="map"
-          style={{
+          sx={{
             height: "350px",
             width: "100%",
-            marginBottom: "20px",
+            marginTop: 2,
+            borderRadius: 1,
+            overflow: "hidden",
+            border: "1px solid #ccc",
           }}
-        ></div>
-      </div>
-    );
-  }
-
-  const post_user = async () => {
-    try {
-      const { provider_name, email, password } = formik.values;
-
-      const new_provider = {
-        name: provider_name,
-        latitude: position.lat,
-        longitude: position.lng,
-      };
-
-      const provider_posted = await Provider_services.post_provider(
-        new_provider
-      );
-      if (!provider_posted) {
-        console.error("Error al añadir proveedor");
-        return;
-      }
-      const new_user_id = provider_posted.provider.id;
-      const new_user = {
-        mail: email,
-        password: password,
-        rol: "Proveedor",
-        client_id: null,
-        provider_id: new_user_id,
-      };
-      const user_posted = await User_services.post_user(new_user);
-      if (!user_posted) {
-        return console.error(error);
-      }
-      console.log("Usuarios Posteados", user_posted);
-    } catch (error) {
-      console.error("Error en post_user:", error);
-    }
-  };
-
-  return (
-    <div className="login-container">
-      <form onSubmit={formik.handleSubmit} className="login-form">
-        <h2 className="form-title">Registro </h2>
-
-        {/* Campo de Correo */}
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Correo"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-          margin="normal"
         />
-
-        {/* Campo de Contraseña */}
-        <TextField
-          fullWidth
-          id="password"
-          name="password"
-          label="Contraseña"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-          margin="normal"
-        />
-
-        {/* Campo de Nombre */}
-        <TextField
-          fullWidth
-          id="provider_name"
-          name="provider_name"
-          label="Nombre"
-          value={formik.values.provider_name}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.provider_name && Boolean(formik.errors.provider_name)}
-          helperText={formik.touched.provider_name && formik.errors.provider_name}
-          margin="normal"
-        />
-
-        {/* Botón de Enviar */}
-        <Button
-          color="primary"
-          variant="contained"
-          fullWidth
-          type="submit"
-          style={{ marginTop: "20px" }}
-        >
-          Enviar
-        </Button>
-      </form>
-      <div className="map_container">{Display_map()}</div>
-      <ToastContainer />
-    </div>
+        <ToastContainer />
+      </Box>
+    </Container>
   );
 };
 
-export default Login_provedor;
+export default Registro_provedor;
