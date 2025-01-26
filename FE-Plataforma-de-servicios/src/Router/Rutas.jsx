8dd15d5e556b90566a791adb5501ff4bd8841ae9
alexia19cahill <alexianavarro2005@gmail.com>
2025-01-26 08:35:58 -0600

    avance homecliente

--- FE-Plataforma-de-servicios/src/Router/Rutas.jsx
+++ FE-Plataforma-de-servicios/src/Router/Rutas.jsx
@@ -5,6 +5,7 @@ import  Proveedor_registro from "../pages/Proveedor_registro";
 import  Clientes_Registro  from "../pages/Clientes_registro";
 import Pagina_proveedor from "../pages/Pagina_proveedor";
 import Servicios from "../pages/Servicios";
+import  Clientes_page from "../pages/Cliente_home";
 
 const Rutas = () => {
   return (
@@ -15,6 +16,7 @@ const Rutas = () => {
         <Route path="/clientes" element={<Clientes_Registro/>} /> 
         <Route path="/pageprove" element={<Pagina_proveedor/>} /> 
         <Route path="/servicios" element={<Servicios />} />
+        <Route path="/homecliente" element={<Clientes_page />} />
     </Routes>
     </>
   );
--- FE-Plataforma-de-servicios/src/pages/Cliente_home.jsx
+++ FE-Plataforma-de-servicios/src/pages/Cliente_home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import  Navbar_cliente from '../componentes/Cliente_home/Navbar_cliente'
 import  Sidebar_cliente  from '../componentes/Cliente_home/Sidebar_cliente'
+import  SearchForm   from '../componentes/Cliente_home/search_cliente'
 const Cliente_page= () => {
   return (
     <div>
         <Navbar_cliente/>
         <Sidebar_cliente/>
+        <SearchForm/>
     </div>
   )
 }
